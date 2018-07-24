9edf7dc40107a3340e8e1d6a2e5eecadc7e05541
Luke Foreman <luke.4man@btinternet.com>
2018-07-24 17:18:54 +0100

    change to singleton

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,86 @@
 import "reflect-metadata";
 
-
-const formatMetadataKey = "format:x";
-
-
-
 function mapFrom(mapFunc: (self: any) => any) {
-    return Reflect.metadata("mapFrom", mapFunc);
+  return Reflect.metadata("mapFrom", mapFunc);
+}
+
+namespace Mapper {
+  const KnownMaps = {};
+
+  export const AddMap = (key, map) => (KnownMaps[key] = map);
+  export const PreformMap = (originalObject, target, mapKey): any => {
+    const map = KnownMaps[mapKey];
+
+    map.forEach(({ expression, key }) => {
+      target[key] = expression(originalObject);
+    });
+    return target;
+  };
 }
 
 class Point {
-    public constructor()
-    {
-        this.x = 5;
-        this.y = 3;
-    }
-    @mapFrom(x => "hello")
-    x: number;
-    y: number;
+  public constructor() {
+    this.x = 5;
+    this.y = 3;
+  }
+  @mapFrom(x => "hello")
+  x: number;
+  y: number;
 }
 const p = new Point();
 
-// function map(t: any): any {
-//     t.prototype.map = (target) => {
-//         const originalObject = this;
-//         console.log(this, originalObject)
-        
-        
-//         for (const k of Reflect.ownKeys(target))
-//         {
-//             const key = <string>k;
-//             let mapFrom: (self) => any = (Reflect.getMetadata("mapFrom", originalObject, key));
-//             const original = (<any>originalObject)[key];
-//             if (original == undefined) {
-//                 throw "Unmapped object";
-//             }
-//             if (!mapFrom) {
-//                 mapFrom = (self) => original;
-//             }
-//             target[key] = mapFrom(originalObject);
-//         }
-//         return target;
-//     };
-// }
+const mappable = ({ originalObject, target, mapKey }): any => something => {
+  console.log(something);
+  const expressions = [];
 
-const map = <T extends {new(...args:any[]):{}}>(constructor:T) => {
-    return class extends constructor {
-        map = (target) => {
-            for (const k of Reflect.ownKeys(target))
-            {
-                const key = <string>k;
-                let mapFrom: (self) => any = (Reflect.getMetadata("mapFrom", this, key));
-                const original = (<any>this)[key];
-                if (original == undefined) {
-                    throw "Unmapped object";
-                }
-                if (!mapFrom) {
-                    mapFrom = (self) => original;
-                }
-                target[key] = mapFrom(this);
-            }
-            return target;
-        };
+  for (const k of Reflect.ownKeys(target)) {
+    const key = <string>k;
+    let mapFrom: (self) => any = Reflect.getMetadata(
+      "mapFrom",
+      originalObject,
+      key
+    );
+
+    if (!mapFrom) {
+      const originalMap = self => (<any>self)[key];
+      if (originalMap(originalObject) == undefined) {
+        throw "Unmapped object";
+      }
+      mapFrom = originalMap;
     }
-}
+    expressions.push({ expression: mapFrom, key });
+    // target[key] = mapFrom(originalObject);
+  }
+  Mapper.AddMap(mapKey, expressions);
+};
 
 interface Mapable {
-    map?: () => void;
-}
-
-@map
-class Point2 {
-    constructor() {
-        this.x = 5;
-        this.y = 7;
-    }
-
-    @mapFrom(self => self.x + 5)
-    x: number;
-    y: number;
-    map: (target: any) => any;
+  map?: () => void;
 }
 
 class Point3 {
-    x: number = null;
-    y: number = null;
-    // z: number = undefined;
+  x: number = null;
+  y: number = null;
+  // z: number = undefined;
 }
 
-// const test = {x: 3, y: 2};
+@mappable({
+  originalObject: new Point2(),
+  target: new Point3(),
+  mapKey: "p2top3"
+})
+class Point2 {
+  constructor() {
+    this.x = 5;
+    this.y = 7;
+  }
 
+  @mapFrom(self => self.x + 3)
+  x: number;
+  y: number;
+  map: (original: any, target: any) => any;
+}
 
 const p2 = new Point2();
-// console.log(Point2.prototype.map); console.log(Point2.prototype.map.bind(test))
-// console.log("test result", Point2.prototype.map(new Point3()).bind(test)());
-
-console.log(p2);
-console.log(p2.map(new Point3()));
-
-const blah = p2.x;
-
+const result = Mapper.PreformMap({ x: 3, y: 15 }, new Point3(), "p2top3");
+console.log(result);
